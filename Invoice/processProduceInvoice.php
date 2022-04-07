81aa24cfa97bf052c1f6a48b1b171be63b0c5d73
Waleed Hassan <waleed.hassan.2@city.ac.uk>
2022-04-07 22:00:32 +0100

    added invoice section (payment remaining)

--- Invoice/processProduceInvoice.php
+++ Invoice/processProduceInvoice.php
@@ -1,4 +1,5 @@
 <?php
+
 // Initialize the session
 session_start();
 require_once "../config.php";
@@ -11,14 +12,14 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 if (isset($_POST['Create'])) {
     $job_id = $_POST['Create'];
-}
 
 $query3 = "SELECT job_id,job_type,status,estimate_amount,book_in_date, time_spent, customer_id, registration_number, username FROM Job  WHERE job_id = '$job_id'";
 $result3 = mysqli_query($conn, $query3);
 while ($row = mysqli_fetch_array($result3)){
     $mechanicUsername = $row['username'];
     $time_spent = $row['time_spent'];
-
+    $customer_id = $row['customer_id'];
+    $job_type = $row['job_type'];
 }
 
 $query = "SELECT hourly_rate FROM Mechanic  WHERE username = '$mechanicUsername'";
@@ -29,30 +30,57 @@ while ($row = mysqli_fetch_array($result)){
 echo "Hourly rate\n";
 echo $hourly_rate;
 $totalMechanic = $hourly_rate * $time_spent;
-$query = "SELECT item_id FROM Stock_used  WHERE job_id = '$job_id'";
-$result = mysqli_query($conn, $query);
-
-while ($row = mysqli_fetch_array($result))
-    $item_id[] = $row['item_id'];
-
-for($i = 0;$i<count($item_id);$i++){
-    echo "Item id\n";
-    echo $item_id[$i];
-    $query = "SELECT price FROM Stock  WHERE item_id = '$item_id[$i]'";
+if($job_type=='repair'){
+    $query = "SELECT item_id FROM Stock_used  WHERE job_id = '$job_id'";
     $result = mysqli_query($conn, $query);
-    while ($row = mysqli_fetch_array($result)){
-        $prices[] = $row['price'];
-    }
-}
 
-for($i = 0;$i<count($prices);$i++){
-    if($i<count($prices)-1)
-        $totalPartsPrice = $prices[$i]+$prices[$i+1];
+    while ($row = mysqli_fetch_array($result))
+        $item_id[] = $row['item_id'];
+
+    for($i = 0;$i<count($item_id);$i++){
+        echo "Item id\n";
+        echo $item_id[$i];
+        $query = "SELECT price FROM Stock  WHERE item_id = '$item_id[$i]'";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_array($result)){
+            $prices[] = $row['price'];
+        }
+    }
+
+    for($i = 0;$i<count($prices);$i++){
+        if($i<count($prices)-1)
+            $totalPartsPrice = $prices[$i]+$prices[$i+1];
+    }
+    $totalPartsPrice *= 1.30;
+    $totalPartsPrice *= 1.20;
+    $totalMechanic *= 1.20;
+    $totalJobWithVat = $totalMechanic+$totalPartsPrice;
+
+   
 }
-echo $totalPartsPrice;
-$totalJob = $totalMechanic+$totalPartsPrice;
 $query2 = "SELECT job_id,job_type,status,estimate_amount,book_in_date, time_spent, customer_id, registration_number, username FROM Job  WHERE job_id = '$job_id'";
 $result2 = mysqli_query($conn, $query2);
+$today = date("Y/m/d");
+$query = "INSERT INTO Invoice (date_created,is_paid,job_id,customer_id) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($query);
+$is_paid = 0;
+$stmt->bind_param('siii',$today,$is_paid,$job_id,$customer_id);
+/* Execute the statement */
+$stmt->execute();
+$row = $stmt->affected_rows;
+if ($row > 0) { 
+    echo "<script type='text/javascript'>alert('Invoice Created');</script>";
+} else {
+    "error";
+}
+$query5 = "SELECT is_paid, date_paid, date_created FROM Invoice  WHERE job_id = '$job_id'";
+$result5 = mysqli_query($conn, $query5);
+while ($row = mysqli_fetch_array($result5)){
+    $is_paid = $row['is_paid'];
+    $date_paid = $row['date_paid'];
+    $date_created = $row['date_created'];
+
+}  
 ?>
 
 <!DOCTYPE html>
@@ -72,6 +100,7 @@ $result2 = mysqli_query($conn, $query2);
         <a href="../<?php echo $role ?>.php" class="btn btn-info ml-3">Open Dashboard</a>
         <meta charset="UTF-8">
         <?php
+        if($job_type == 'repair'){
         echo "<h3 class='my-5'>Completed Jobs</h1>";
         echo "<div class='container'>";
         echo "<div class='row-fluid'>";
@@ -88,18 +117,17 @@ $result2 = mysqli_query($conn, $query2);
                 echo "<th>Total parts price</th>";
                 echo "<th>Mechanic rate</th>";
                 echo "<th>Total mechanic</th>";
-                echo "<th>Total job amount</th>";
+                echo "<th>Total with VAT</th>";
                 echo "<th>Customer Id</th>";
                 echo "<th>Registration Number</th>";
                 echo "<th>Mechanic Id</th>";
-                echo "<th>Date</th>";
+                echo "<th>Date Created</th>";
                 echo "<th>Paid</th>";
                 echo "</tr>";
-
                 if ($result2->num_rows > 0) {
                     // output data of each row
                     while($row = $result2->fetch_assoc()) {
-                        echo"<form action = 'processProduceInvoice.php' method='POST'>";  
+                        echo"<form action = 'payInvoice.php' method='POST'>";  
                         echo "<tr>";
                         echo "<td>" . $row["job_id"] . "</td>";
                         echo "<td>" . $row["job_type"] . "</td>";
@@ -110,12 +138,70 @@ $result2 = mysqli_query($conn, $query2);
                         echo "<td>" . $totalPartsPrice. "</td>";
                         echo "<td>" . $hourly_rate. "</td>";
                         echo "<td>" . $totalMechanic. "</td>";
-                        echo "<td>" . $totalJob. "</td>";
+                        echo "<td>" . $totalJobWithVat. "</td>";
                         echo "<td>".$row['customer_id']."</td>";
                         echo "<td>".$row['registration_number']."</td>";
                         echo "<td>".$row['username']."</td>";
-                        echo "<td>2022</td>";
-                        echo "<td>yes</td>";
+                        echo "<td>$date_created</td>";
+                        if($is_paid==0)
+                            echo "<td><input type='submit' name='pay' value='" . $row['job_id'] . "' /><br/>Pay</td>";
+                        else
+                            echo "<td>Yes on.$date_paid.</td>";
+                        echo "</tr>";
+                echo"</form>";           
+                    }
+                } else {
+                    echo "0 results";
+                }
+                
+                echo "</table>";
+    
+            echo "</div>";
+            echo "</div>";
+        }
+        else{
+            echo "<h3 class='my-5'>Completed Jobs</h1>";
+        echo "<div class='container'>";
+        echo "<div class='row-fluid'>";
+            echo "<div class='col-xs-12'>";
+            echo "<div class='table-responsive'>";    
+                echo "<table class='table table-hover table-inverse'>";
+                echo "<tr>";
+                echo "<th>Job id</th>";
+                echo "<th>Job type</th>";
+                echo "<th>Status</th>";
+                echo "<th>Estimate amount</th>";
+                echo "<th>Book in date</th>";
+                echo "<th>Total with VAT</th>";
+                echo "<th>Customer Id</th>";
+                echo "<th>Registration Number</th>";
+                echo "<th>Mechanic Id</th>";
+                echo "<th>Date Created</th>";
+                echo "<th>Paid</th>";
+                echo "</tr>";
+
+                if ($result2->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result2->fetch_assoc()) {
+                        echo"<form action = 'payInvoice.php' method='POST'>";  
+                        echo "<tr>";
+                        echo "<td>" . $row["job_id"] . "</td>";
+                        echo "<td>" . $row["job_type"] . "</td>";
+                        echo "<td>" . $row["status"] . "</td>";
+                        echo "<td>" . $row["estimate_amount"] . "</td>";
+                        echo "<td>" . $row['book_in_date'] . "</td>";
+                        if($job_type=='MoT')
+                            echo "<td>66£</td>";
+                        else
+                            echo "<td>114£</td>";
+                        echo "<td>".$row['customer_id']."</td>";
+                        echo "<td>".$row['registration_number']."</td>";
+                        echo "<td>".$row['username']."</td>";
+                        echo "<td>$date_created</td>";
+                        if($is_paid==0)
+                            echo "<td><input type='submit' name='pay' value='" . $row['job_id'] . "' /><br/>Pay</td>";
+                        else
+                            echo "<td>Yes on.$date_paid.</td>";
                         echo "</tr>";
                         echo"</form>";           
                     }
@@ -127,5 +213,195 @@ $result2 = mysqli_query($conn, $query2);
     
             echo "</div>";
             echo "</div>";
+        }
 
+    }
 
+    else{
+        $job_id = $_POST['View'];
+
+        $query3 = "SELECT job_id,job_type,status,estimate_amount,book_in_date, time_spent, customer_id, registration_number, username FROM Job  WHERE job_id = '$job_id'";
+    $result3 = mysqli_query($conn, $query3);
+    while ($row = mysqli_fetch_array($result3)){
+        $mechanicUsername = $row['username'];
+        $time_spent = $row['time_spent'];
+        $customer_id = $row['customer_id'];
+        $job_type = $row['job_type'];
+    }
+
+    $query = "SELECT hourly_rate FROM Mechanic  WHERE username = '$mechanicUsername'";
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_array($result)){
+        $hourly_rate = $row['hourly_rate'];
+    }  
+    echo "Hourly rate\n";
+    echo $hourly_rate;
+    $totalMechanic = $hourly_rate * $time_spent;
+    if($job_type=='repair'){
+        $query = "SELECT item_id FROM Stock_used  WHERE job_id = '$job_id'";
+        $result = mysqli_query($conn, $query);
+
+        while ($row = mysqli_fetch_array($result))
+            $item_id[] = $row['item_id'];
+
+        for($i = 0;$i<count($item_id);$i++){
+            echo "Item id\n";
+            echo $item_id[$i];
+            $query = "SELECT price FROM Stock  WHERE item_id = '$item_id[$i]'";
+            $result = mysqli_query($conn, $query);
+            while ($row = mysqli_fetch_array($result)){
+                $prices[] = $row['price'];
+            }
+        }
+
+        for($i = 0;$i<count($prices);$i++){
+            if($i<count($prices)-1)
+                $totalPartsPrice = $prices[$i]+$prices[$i+1];
+        }
+        $totalPartsPrice *= 1.30;
+        $totalPartsPrice *= 1.20;
+        $totalMechanic *= 1.20;
+        $totalJobWithVat = $totalMechanic+$totalPartsPrice;
+
+    }
+    $query2 = "SELECT job_id,job_type,status,estimate_amount,book_in_date, time_spent, customer_id, registration_number, username FROM Job  WHERE job_id = '$job_id'";
+    $result2 = mysqli_query($conn, $query2);
+
+    $query5 = "SELECT is_paid, date_paid, date_created FROM Invoice  WHERE job_id = '$job_id'";
+    $result5 = mysqli_query($conn, $query5);
+    while ($row = mysqli_fetch_array($result5)){
+        $is_paid = $row['is_paid'];
+        $date_paid = $row['date_paid'];
+        $date_created = $row['date_created'];
+    }  
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <head>
+    <style>
+            body{text-align: center; }
+    </style>
+    <body>
+        <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to GARITS.</h1>
+        <p>
+            <a href="../logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+            <a href="../<?php echo $role ?>.php" class="btn btn-info ml-3">Open Dashboard</a>
+            <meta charset="UTF-8">
+            <?php
+            if($job_type == 'repair'){
+            echo "<h3 class='my-5'>Completed Jobs</h1>";
+            echo "<div class='container'>";
+            echo "<div class='row-fluid'>";
+                echo "<div class='col-xs-12'>";
+                echo "<div class='table-responsive'>";    
+                    echo "<table class='table table-hover table-inverse'>";
+                    echo "<tr>";
+                    echo "<th>Job id</th>";
+                    echo "<th>Job type</th>";
+                    echo "<th>Status</th>";
+                    echo "<th>Estimate amount</th>";
+                    echo "<th>Book in date</th>";
+                    echo "<th>Time Spent</th>";
+                    echo "<th>Total parts price</th>";
+                    echo "<th>Mechanic rate</th>";
+                    echo "<th>Total mechanic</th>";
+                    echo "<th>Total with VAT</th>";
+                    echo "<th>Customer Id</th>";
+                    echo "<th>Registration Number</th>";
+                    echo "<th>Mechanic Id</th>";
+                    echo "<th>Date Created</th>";
+                    echo "<th>Paid</th>";
+                    echo "</tr>";
+                    if ($result2->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result2->fetch_assoc()) {
+                            echo"<form action = 'payInvoice.php' method='POST'>";  
+                            echo "<tr>";
+                            echo "<td>" . $row["job_id"] . "</td>";
+                            echo "<td>" . $row["job_type"] . "</td>";
+                            echo "<td>" . $row["status"] . "</td>";
+                            echo "<td>" . $row["estimate_amount"] . "</td>";
+                            echo "<td>" . $row['book_in_date'] . "</td>";
+                            echo "<td>" . $row['time_spent'] . "</td>";
+                            echo "<td>" . $totalPartsPrice. "</td>";
+                            echo "<td>" . $hourly_rate. "</td>";
+                            echo "<td>" . $totalMechanic. "</td>";
+                            echo "<td>" . $totalJobWithVat. "</td>";
+                            echo "<td>".$row['customer_id']."</td>";
+                            echo "<td>".$row['registration_number']."</td>";
+                            echo "<td>".$row['username']."</td>";
+                            echo "<td>$date_created</td>";
+                                echo "<td>Yes on.$date_paid.</td>";
+                            echo "</tr>";
+                    echo"</form>";           
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    
+                    echo "</table>";
+        
+                echo "</div>";
+                echo "</div>";
+            }
+            else{
+                echo "<h3 class='my-5'>Completed Jobs</h1>";
+            echo "<div class='container'>";
+            echo "<div class='row-fluid'>";
+                echo "<div class='col-xs-12'>";
+                echo "<div class='table-responsive'>";    
+                    echo "<table class='table table-hover table-inverse'>";
+                    echo "<tr>";
+                    echo "<th>Job id</th>";
+                    echo "<th>Job type</th>";
+                    echo "<th>Status</th>";
+                    echo "<th>Estimate amount</th>";
+                    echo "<th>Book in date</th>";
+                    echo "<th>Total with VAT</th>";
+                    echo "<th>Customer Id</th>";
+                    echo "<th>Registration Number</th>";
+                    echo "<th>Mechanic Id</th>";
+                    echo "<th>Date Created</th>";
+                    echo "<th>Paid</th>";
+                    echo "</tr>";
+
+                    if ($result2->num_rows > 0) {
+                        // output data of each row
+                        while($row = $result2->fetch_assoc()) {
+                            echo"<form action = 'payInvoice.php' method='POST'>";  
+                            echo "<tr>";
+                            echo "<td>" . $row["job_id"] . "</td>";
+                            echo "<td>" . $row["job_type"] . "</td>";
+                            echo "<td>" . $row["status"] . "</td>";
+                            echo "<td>" . $row["estimate_amount"] . "</td>";
+                            echo "<td>" . $row['book_in_date'] . "</td>";
+                            if($job_type=='MoT')
+                                echo "<td>66£</td>";
+                            else
+                                echo "<td>114£</td>";
+                            echo "<td>".$row['customer_id']."</td>";
+                            echo "<td>".$row['registration_number']."</td>";
+                            echo "<td>".$row['username']."</td>";
+                            echo "<td>$date_created</td>";
+  
+                                echo "<td>Yes on.$date_paid.</td>";
+                            echo "</tr>";
+                            echo"</form>";           
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    
+                    echo "</table>";
+        
+                echo "</div>";
+                echo "</div>";
+            }
+
+        }
+        
